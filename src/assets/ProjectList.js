55f0fe9c1bda8fe045455919cf439667ef49eeaf
steliosMertzanis <mertzanisst@gmail.com>
2023-10-18 23:17:47 +0300

    v2

--- src/assets/ProjectList.js
+++ src/assets/ProjectList.js
@@ -4,80 +4,25 @@ import HomePage803 from "./images/HomePage803.jpg";
 
 export const ProjectList = [
   {
-    name: "Home Page",
+    name: "ToDo List",
     image: {
       small: HomePage320,
       medium: HomePage640,
       large: HomePage803,
     },
-    url: "https://steliosmertzanis.github.io/draft/",
-    repositoryUrl: "https://github.com/steliosMertzanis/draft.git",
+    url: "https://steliosmertzanis.github.io/todo-list/",
+    repositoryUrl: "https://github.com/steliosMertzanis/todo-list",
     description: "use of react",
   },
   {
-    name: "Home Page1",
+    name: "Form Sign Up",
     image: {
       small: HomePage320,
       medium: HomePage640,
       large: HomePage803,
     },
-    url: "https://steliosmertzanis.github.io/draft/",
-    repositoryUrl: "https://github.com/steliosMertzanis/draft.git",
-    description: "use of react",
-  },
-  {
-    name: "Home Page2",
-    image: {
-      small: HomePage320,
-      medium: HomePage640,
-      large: HomePage803,
-    },
-    url: "https://steliosmertzanis.github.io/draft/",
-    repositoryUrl: "https://github.com/steliosMertzanis/draft.git",
-    description: "use of react",
-  },
-  {
-    name: "Home Page3",
-    image: {
-      small: HomePage320,
-      medium: HomePage640,
-      large: HomePage803,
-    },
-    url: "https://steliosmertzanis.github.io/draft/",
-    repositoryUrl: "https://github.com/steliosMertzanis/draft.git",
-    description: "use of react",
-  },
-  {
-    name: "Home Page4",
-    image: {
-      small: HomePage320,
-      medium: HomePage640,
-      large: HomePage803,
-    },
-    url: "https://steliosmertzanis.github.io/draft/",
-    repositoryUrl: "https://github.com/steliosMertzanis/draft.git",
-    description: "use of react",
-  },
-  {
-    name: "Home Page5",
-    image: {
-      small: HomePage320,
-      medium: HomePage640,
-      large: HomePage803,
-    },
-    url: "https://steliosmertzanis.github.io/draft/",
-    repositoryUrl: "https://github.com/steliosMertzanis/draft.git",
-    description: "use of react",
-  },
-  {
-    name: "Home Page6",
-    image: {
-      small: HomePage320,
-      medium: HomePage640,
-      large: HomePage803,
-    },
-    url: "https://steliosmertzanis.github.io/draft/",
-    repositoryUrl: "https://github.com/steliosMertzanis/draft.git",
+    url: "https://steliosmertzanis.github.io/form-website/",
+    repositoryUrl: "https://github.com/steliosMertzanis/form-website",
     description: "use of react",
   },
 ];
